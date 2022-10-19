2de5013dc4b13f238c2ec2b18bb2ebe5c564c557
Vladimir <user@MAK.local>
2022-10-19 20:34:06 +0700

    возводим в степень

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.WriteLine("Введите число 1");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2");
+int number2 = int.Parse(Console.ReadLine()!);
+double degree = 0;
+degree = Math.Pow(number1, number2);
+Console.WriteLine($"итого {degree}");
